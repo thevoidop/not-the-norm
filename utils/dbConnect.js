7284ac99acfff03d2a51cc1d148678314baf82d9
thevoidop <mail.voidop@gmail.com>
2025-05-26 23:17:40 +0530

    fixed bugs

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -6,10 +6,7 @@ async function connect() {
     if (isConnected) return;
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI, {});
         isConnected = true;
         console.log("MongoDB connected");
     } catch (error) {
